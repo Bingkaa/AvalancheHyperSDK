9876c1fe5dd790a8b820d570540eac67768daebb
unknown <goldman.bianca@gmail.com>
2023-11-01 23:14:15 +0800

    first commit

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Bingkoin"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Bianca"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "BGK"
 )
 
 var ID ids.ID
